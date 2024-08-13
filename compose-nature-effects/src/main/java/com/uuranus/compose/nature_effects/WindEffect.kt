602b9cb50d5ad45aae8c7939c767f65b3658c6dc
uuranus <hyur0920@gmail.com>
2024-08-13 13:58:19 +0900

    feat: WindBlownDiagonalEffect

--- compose-nature-effects/src/main/java/com/uuranus/compose/nature_effects/WindEffect.kt
+++ compose-nature-effects/src/main/java/com/uuranus/compose/nature_effects/WindEffect.kt
@@ -6,8 +6,12 @@ import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -15,17 +19,34 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.android.awaitFrame
 import kotlin.math.PI
 import kotlin.math.cos
+import kotlin.math.roundToInt
 import kotlin.math.sin
 import kotlin.random.Random
 
@@ -120,7 +141,9 @@ data class Light(
 )
 
 @Composable
-fun WindBlownDiagonalEffect() {
+fun WindBlownDiagonalEffect(
+    resourceId: Int,
+) {
 
     val configuration = LocalConfiguration.current
     val density = LocalDensity.current
@@ -135,25 +158,28 @@ fun WindBlownDiagonalEffect() {
 
     var things by remember {
         mutableStateOf(
-            List(20) {
+            List(10) {
                 val x = Random.nextInt(screenWidthPx.toInt()).toFloat()
                 val y = Random.nextInt(screenHeightPx.toInt() / 2).toFloat()
 
-                if (Random.nextInt(2) == 1) {
+                val offset = if (Random.nextInt(2) == 1) {
                     Offset(x, -y)
                 } else {
                     Offset(screenWidthPx + x, y)
                 }
+
+                val imageSize = Random.nextInt(10, 40).dp
+                Leaf(offset, imageSize, 0f)
             })
     }
 
     LaunchedEffect(Unit) {
         while (true) {
-            things = things.map { offset ->
-                val newX = offset.x - 10f // Adjust speed by changing the value
-                val newY = offset.y + 10f // Adjust speed by changing the value
+            things = things.map { thing ->
+                val newX = thing.offset.x - 10f
+                val newY = thing.offset.y + 10f
 
-                if (newX <= 0 || newY >= screenHeightPx) {
+                val offset = if (newX <= 0 || newY >= screenHeightPx) {
 
                     val x = Random.nextInt(screenWidthPx.toInt()).toFloat()
                     val y = Random.nextInt(screenHeightPx.toInt() / 2).toFloat()
@@ -163,38 +189,53 @@ fun WindBlownDiagonalEffect() {
                     } else {
                         Offset(screenWidthPx + x, y)
                     }
-
                 } else {
                     Offset(newX, newY)
                 }
+
+                val rotate = (thing.rotate + 2) % 360
+                thing.copy(offset = offset, rotate = rotate)
             }
-            // Add a delay for smooth animation
-            kotlinx.coroutines.delay(100L) // Roughly 60fps
+
+            kotlinx.coroutines.delay(100L)
         }
     }
 
-    Canvas(
+    val imageVector = ImageBitmap.imageResource(id = resourceId)
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black)
+            .background(Color.Transparent)
     ) {
 
         for (thing in things) {
-            drawCircle(
-                brush = Brush.radialGradient(
-                    colors = listOf(Color.White, Color.Transparent),
-                    center = thing,
-                    radius = 9.dp.toPx()
-                ),
-                center = thing,
-            )
-            drawCircle(
-                color = Color.White,
-                center = thing,
-                radius = 5.dp.toPx()
+
+            val offsetX = with(density) {
+                thing.offset.x.toDp()
+            }
+            val offsetY = with(density) {
+                thing.offset.y.toDp()
+            }
+
+            Image(
+                bitmap = imageVector,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(thing.size)
+                    .offset(
+                        x = offsetX,
+                        y = offsetY
+                    )
+                    .rotate(thing.rotate),
+                colorFilter = ColorFilter.tint(color = Color(0xFFAE8B4E))
             )
         }
     }
-
-
 }
+
+data class Leaf(
+    val offset: Offset,
+    val size: Dp,
+    val rotate: Float,
+)
