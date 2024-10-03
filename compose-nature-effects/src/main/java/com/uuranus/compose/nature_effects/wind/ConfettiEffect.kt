a2bf78bcee54a13ab80917188385d95cc6c1fbc6
uuranus <hyur0920@gmail.com>
2024-10-03 20:01:02 +0900

    feat: remove unused method

--- compose-nature-effects/src/main/java/com/uuranus/compose/nature_effects/wind/ConfettiEffect.kt
+++ compose-nature-effects/src/main/java/com/uuranus/compose/nature_effects/wind/ConfettiEffect.kt
@@ -29,10 +29,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -236,14 +234,6 @@ data class Confetti(
                     color = color.copy(alpha = alpha.value),
                     shape = shape
                 ))
-    }
-
-    fun draw(drawScope: DrawScope) {
-        drawScope.drawRect(
-            color = color.copy(alpha = alpha.value),
-            topLeft = offset.value,
-            size = Size(10f, 20f),
-        )
     }
 }
 
